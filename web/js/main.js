4f55b7bc9e3c9e00190df1a6adef62c1f995e6da
Sergey <sergeypanic@gmail.com>
2015-05-23 14:39:16 +0300

    check axeses

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,82 @@
-var width = 700;
-var height = 300;
+var width = $('body').width();
+var height = 400;
+
+var dataLength = data.length;
 
 var svg = d3.select('body')
 			.append('svg')
 			.attr('width', width)
 			.attr('height', height);
 
+var xScale = d3.scale.linear()
+					.domain([d3.min(data, function(d){
+						return d.death_year
+					}), d3.max(data, function(d){
+						return d.death_year
+					})])
+					.range([0, width]);
+
+var yScale = d3.scale.linear()
+					.domain([d3.min(data, function(d){
+						return d.age
+					}), d3.max(data, function(d){
+						return d.age
+					})])
+					.range([0, height]);
+
+var xAxis = function() {
+			return d3.svg.axis()
+			.scale(xScale)
+			.ticks(5)
+			.innerTickSize(-height)
+			.outerTickSize(0)
+			.tickFormat("");
+		};
+
+var yAxis = function() {
+			return d3.svg.axis()
+			.scale(yScale)
+			.ticks(5)
+			.orient("left")
+			.innerTickSize(-width)
+			.outerTickSize(0)
+			.tickFormat("");
+		};
+
+svg.append("svg:g")
+			.attr("class", "grid")
+			.attr("transform", "translate(10,10)")
+			.call(xAxis().ticks(10));
+
+svg.append("svg:g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(10,10)")
+			.call(xAxis());
+
 svg.selectAll('rect')
 	.data(data)
 	.enter()
 	.append('rect')
-	.attr('width','10px')
-	.attr('height','10px')
-	.classed('square', true)
-	.attr('x', function(d) {
-		return d.death_year - 1500
+	.classed('bar',true)
+	.attr('width',10)
+	.attr('height',function(d){
+		return yScale(d.age)
 	})
+	.attr('x',function(d){
+		return xScale(d.death_year)
+	});
+
+svg.selectAll('text')
+	.data(data)
+	.enter()
+	.append('text')
+	.attr('x',function(d){
+		return xScale(d.death_year)
+	})
+	.attr('y',function(d){
+		return yScale(d.age) + 10
+	})
+	.classed('name-text',true)
+	.text(function(d){
+		return d.name + " " + d.age
+	});
